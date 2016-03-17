1d0b0f6b9d88e4306e1647e5c4cabf708f045502
kunok <kunokdev@gmail.com>
2016-03-17 22:36:54 +0100

    ...

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,10 +5,10 @@ class Post < ActiveRecord::Base
   has_many :edits
   has_many :comments
 
+
   default_scope -> { order created_at: :desc }
   validates :user_id, presence: true
   validates :content,
     presence: true,
     length: {maximum: 200}
-
 end
